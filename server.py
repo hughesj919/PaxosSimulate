627544ad7f71a5b4cc08660393ef9163d3706950
Daniel Imberman <danielryan2430@gmail.com>
2014-12-08 20:33:19 -0800

    hopefully mods work

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import click
 import pickle
 import os
 import errno
+import json
 import thread
 import math
 from socket import error as socket_error
@@ -17,12 +18,17 @@ bufferSize = 1024
 ballotNum = 0.0
 acceptNum = None
 acceptVal = None
+potentialExtras = []
 myProposal = None
+modMyProposal = None
 nodeid = 0
 prepAckCount = 0
 origConn = None
 failure = False
 repropose = True
+appenders = []
+useAppenders = False
+needsUpdate = False
 
 s1 = '54.173.225.121'
 s2 = '54.172.165.23'
@@ -34,15 +40,17 @@ servers = [s1, s2, s3, s4, s5]
 
 
 @click.command()
-@click.option('-m', help='Runs program using modified ISPaxos implementation.')
-def main(m):
+@click.option('-mod', help='Runs program using modified ISPaxos implementation.')
+def main(mod):
+    global m
+    m = mod
     getnodeid()
     setfailure(False)
     loadlog()
     # updateLog()  #this needs to be the first thing done since a node could be recovering from crash failure
     listen()  # fire up the server
 
-# this listens continunally on the main thread
+# this listens continually on the main thread
 def listen():
     # start up the tcp server and begin listening for requests
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -56,7 +64,7 @@ def listen():
 
 # this will use a new thread to handle incoming data everytime
 def handledata(conn, addr):
-    global ballotNum, acceptNum, acceptVal, prepAckCount, origConn, acceptors
+    global ballotNum, acceptNum, acceptVal, prepAckCount, origConn, acceptors, appenders
     while True:
             data = conn.recv(bufferSize)
             # this is where we should do different things based on the data received, i.e. if balance request, then return balance, etc.
@@ -88,7 +96,12 @@ def handledata(conn, addr):
                 elif cmd[0] == 'accepted':
                     handleAccepted(addr)
                 elif cmd[0] == 'decide':
-                    handleDecide()
+                    if m: modifiedHandleDecide(cmd)
+                    else: handleDecide(cmd)
+                elif cmd[0] == 'update':
+                    handleUpdate(cmd[1])
+                elif cmd[0] == 'updating':
+                    updateLog(cmd[1])
     conn.close()
 
 
@@ -96,7 +109,7 @@ def handlePrepare(cmd,addr):
     global ballotNum, acceptNum, acceptVal
     if float(cmd[1]) > ballotNum:
         ballotNum = float(cmd[1])
-        msg = 'ack' + ' ' + str(ballotNum) + ' ' + str(acceptNum) + ' ' + str(acceptVal)
+        msg = 'ack' + ' ' + str(ballotNum) + ' ' + str(acceptNum) + ' ' + str(acceptVal)+' '+str(getnodeid())
         talk(addr[0], msg)
 
 def handleAck(cmd):
@@ -104,12 +117,15 @@ def handleAck(cmd):
     priorMajority = (prepAckCount >= (len(servers) // 2 + 1))
     prepAckCount += 1
     if cmd[2] != 'None' and cmd[3] != 'None':
-        acceptValues.append((cmd[2], cmd[3] + ' ' + cmd[4]))
+        acceptValues.append((cmd[2], cmd[3] + ' ' + cmd[4] + ' '+cmd[5]))
         # vif we have a quorum start accept phase
     if (prepAckCount >= (len(servers) // 2 + 1)) and not priorMajority:
-        prop = getProposalValue()
+        if m: prop = modifiedGetProposalValue()
+        else: prop = getProposalValue()
         print 'Quorum received, starting acceptance phase with proposal: ' + prop
         accept(ballotNum, prop)
+
+
 
 
 def handleAccept(cmd):
@@ -132,21 +148,7 @@ def handleAccepted(addr):
             decide(acceptVal)
 
 
-def handleDecide():
-    global origConn, acceptVal, myProposal, repropose
-    decision = acceptVal.split()
-    writeDecision(decision)
-    if decision[0] == 'd' and origConn is not None:
-        origConn.send('Deposit ' + str(decision[1]) + ' added to log.')
-    elif decision[0] == 'w' and origConn is not None:
-        origConn.send('Withdraw ' + str(decision[1]) + ' added to log.')
-    resetPaxosValues()
-    if repropose and (myProposal is not None):
-        transactionRequest(myProposal[0], myProposal[1])
-    else:
-        myProposal = None
-        origConn = None
-        repropose = True
+
 
 
 def loadlog():
@@ -154,7 +156,7 @@ def loadlog():
     if not os.path.isfile('log.txt'):
         open('log.txt', 'wb+')
     try:
-        log = pickle.load(open('log.txt', 'rb+'))
+        log = json.load(open('log.txt', 'rb+'))
     except EOFError:
         print 'Log file currently empty...'
 
@@ -175,30 +177,99 @@ def setfailure(val):
     global failure
     failure = val
 
-#def updateLog():
-    # this should loop until it receives a copy of a log from another node, at which point it can update its own log
+def requestUpdate():
+    global needsUpdate
+    for server in servers:
+        talk(server, 'update ' + nodeid)
+        needsUpdate = True
+
+def updateLog(lg):
+    global needsUpdate, log
+    if not needsUpdate:
+        tmp = json.load(lg)
+        if len(tmp)>len(log):
+            log = tmp
+            needsUpdate=False
 
 
 def decide(val):
     for server in servers:
-        talk(server, 'decide ' + val)
+        if m: talk(server, 'decide ' + val + ' ' + json.dumps(potentialExtras) + ' ' + json.dumps(appenders))
+        else: talk(server, 'decide ' + val)
+
+
+def handleDecide(val):
+    global origConn, acceptVal, myProposal, repropose
+    decision = val.split()[1:]
+    writeDecision(decision)
+    if decision[0] == 'd' and origConn is not None:
+        origConn.send('Deposit ' + str(decision[1]) + ' added to log.')
+    elif decision[0] == 'w' and origConn is not None:
+        origConn.send('Withdraw ' + str(decision[1]) + ' added to log.')
+    resetPaxosValues()
+    if repropose and (myProposal is not None):
+        potentialExtras = []
+        appenders = []
+        transactionRequest(myProposal[0], myProposal[1])
+    else:
+        myProposal = None
+        origConn = None
+        repropose = True
+
+def modifiedHandleDecide(val):
+    global origConn, acceptVal, myProposal, repropose, potentialExtras, appenders
+    decision = val.split()[1:]
+    modifiedWriteDecision(decision)
+    if decision[0] == 'd' and origConn is not None:
+        origConn.send('Deposit ' + str(decision[1]) + ' added to log with appended values: ' + potentialExtras)
+    elif decision[0] == 'w' and origConn is not None:
+        origConn.send('Withdraw ' + str(decision[1]) + ' added to log.')
+    resetPaxosValues()
+    if repropose and (myProposal is not None):
+        transactionRequest(myProposal[0], myProposal[1])
+    else:
+        myProposal = None
+        origConn = None
+        repropose = True
+
+
+def modifiedWriteDecision(val):
+    global potentialExtras, appenders
+    potentialExtras = json.loads(val[3])
+    appenders = json.loads(val[4])
+    if(val[0]=='d'):
+        logposition = int(val[2])
+        log[logposition] = (val[0], float(val[1]))
+        logposition+=1
+        for i in potentialExtras:
+            log[logposition] = (val[0], float(i.split()[1]))
+            logposition+=1
+    else:
+        logposition = int(val[2])
+        log[logposition] = (val[0], float(val[1]))
+    pickle.dump(log, open('log.txt', 'wb+'))
+
+
+
+
+
+
 
 def writeDecision(val):
     print 'Decide val:' + val[0] + ' ' + val[1] + ' ' + val[2]
     # if this is the first decide message make space in the log, otherwise just update
-    if len(log) <= int(val[2]):
-        log.append((val[0], float(val[1])))
-    else:
-        log[int(val[2])] = (val[0], float(val[1]))
+    log[int(val[2])] = (val[0], float(val[1]))
     pickle.dump(log, open('log.txt', 'wb+'))
 
 
 def resetPaxosValues():
-    global acceptValues, acceptors, prepAckCount, acceptNum, acceptVal;
+    global acceptValues, acceptors, prepAckCount, acceptNum, acceptVal, modvalues
     acceptValues = []
     acceptors = []
     prepAckCount = 0
     acceptNum = None
+    appenders = []
+    potentialExtras = []
     acceptVal = None
 
 
@@ -229,9 +300,12 @@ def talk(srv, msg):
 
 def transactionRequest(type, amount):
     # this should start a paxos instance, if successful the transaction wins the spot, otherwise this should be called again
-    global myProposal
+    global myProposal, appenders
     if type == 'w' and float(amount) > balancerequest():
         origConn.send('Transaction would result in overdraft. Rejected.')
+    elif m:
+        modvalues.append((amount,getnodeid()))
+        myProposal = (type, modvalues, appenders, str(len(log)))
     else:
         myProposal = (type, amount, str(len(log)))
         propose()
@@ -247,12 +321,21 @@ def propose():
 
 def accept(acceptNum, acceptVal):
     for server in servers:
-        talk(server, 'accept ' + str(acceptNum) + ' ' + str(acceptVal) + ' ' + str(len(log)))
+        # if m:
+        #     if appenders == None: add = ''
+        #     else: add = ' ' + appenders
+        #     talk(server, 'accept' + str(acceptNum) + ' ' + acceptVal[0] + ' ' + str(len(log)) + add)
+        # else:
+            talk(server, 'accept ' + str(acceptNum) + ' ' + str(acceptVal) + ' ' + str(len(log)))
 
 
 def accepted(acceptNum, acceptVal):
     for server in servers:
+        # if m: talk(server, 'accepted ' + str(acceptNum) + ' ' + acceptVal[0] + add)
+        # else:
         talk(server, 'accepted ' + str(acceptNum) + ' ' + str(acceptVal))
+
+
 
 
 def getProposalValue():
@@ -268,6 +351,30 @@ def getProposalValue():
     else:
         return myProposal[0] + ' ' + myProposal[1]
 
+def modifiedGetProposalValue():
+    if len(acceptValues) > 0:
+        maxIsDep = False
+        maxBal = acceptValues[0][0]
+        maxVal = acceptValues[0][1]
+        for val in acceptValues:
+            if myProposal[1][0]=='d' and val[0]=='d':
+                potentialExtras.append(val)
+                appenders.append(val.split(' ')[5])
+            if val[0]>=maxBal:
+                if val[0]=='d':
+                    maxIsDep =True
+                else: maxIsDep = False
+                maxBal = val[0]
+                maxVal = val[1]
+        if maxIsDep:
+            useAppenders = True
+        else:
+            useAppenders = False
+        return maxVal
+    return myProposal[0] + ' ' + myProposal[1]
+
+def handleUpdate(requester):
+    talk(requester,'updating ' + json.dump(log))
 
 def printLog(conn):
     logstr=''
